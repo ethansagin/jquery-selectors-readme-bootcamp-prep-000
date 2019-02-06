426a7a96174705f9604e12e82337bc3db486b6c4
Ethan <ethansagin@gmail.com>
2019-02-06 20:46:11 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,11 +1,11 @@
 // declare your functions here...
 
 function paragraphSelector() {
-  $('p')
+  return $('p')
 }
 
 function lastImageSelector() {
-  $('img:last')
+  return $('img:last')
 }
 
 function ninjaBabySelector() {
